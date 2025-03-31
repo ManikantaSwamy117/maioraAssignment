139286a9b386ab40f3a6cf077063a3a50e0170e6
maniknata <iam.manikanta244@gmail.com>
2025-03-31 14:52:00 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoaderService } from 'src/services/loader.service';
-import Swal from 'sweetalert2';
-import { App as CapacitorApp } from '@capacitor/app';
-import { Browser } from '@capacitor/browser';
-import { Platform } from '@ionic/angular';
-import { FireBasedNotificationsService } from 'src/services/fire-based-notifications.service';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { Router } from '@angular/router';
-import { Location } from '@angular/common';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
